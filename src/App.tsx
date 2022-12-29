9a19d74bb7079ceb39ed3c13ba354670975c8874
xuanhung1509 <vuxuanhung15092002@gmail.com>
2022-12-29 16:41:47 +0700

    style: remove tap highlight color

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,25 @@ import {
 import { PomodoroProvider } from '@/contexts/PomodoroContext';
 
 const App = () => (
-  <PomodoroProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/pomodoro' element={<Pomodoro />} />
-        <Route path='/planner' element={<Planner />} />
-        <Route path='/*' element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </PomodoroProvider>
+  <div
+    style={{
+      WebkitTapHighlightColor: 'transparent',
+    }}
+  >
+    <PomodoroProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/pomodoro' element={<Pomodoro />} />
+          <Route path='/planner' element={<Planner />} />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </PomodoroProvider>
+  </div>
 );
 
 export default App;
